7afbcd4902afd3042dc92f802a7b1c3cb5e644bf
zeylin <ellinam31@gmail.com>
2019-07-22 22:17:45 +0600

    Initial commit

---
+++ src/main/java/com/zeylin/runoncoffee/RunOnCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.zeylin.runoncoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunOnCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunOnCoffeeApplication.class, args);
+	}
+
+}
